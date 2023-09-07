7cb36991fb92a1dbbaa20e553c1271c7d7e3529e
Simon Frändén <simon.franden@hotmail.com>
2023-09-07 12:54:13 +0200

    Add project files.

---
+++ ImageAnalyzer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace SeeFood
+{
+    public class ImageAnalyzer
+    {
+        private static string cognitiveEndpoint = "https://compvisionseefood.cognitiveservices.azure.com/";
+        private static string cognitiveKey = "16fea0ae9134441baf586041c8125192";
+
+        private readonly ComputerVisionClient computerVision;
+
+        public ImageAnalyzer()
+        {
+            ApiKeyServiceClientCredentials visionCredentials = new(cognitiveKey);
+            computerVision = new ComputerVisionClient(visionCredentials);
+            computerVision.Endpoint = cognitiveEndpoint;
+        }
+
+        
+        public async void DectectItemsAsync(string url)
+        {
+            Console.Clear();
+            //Select what to look for in the image
+            List<VisualFeatureTypes?> featureTypes = new()
+            {
+                VisualFeatureTypes.Objects
+            };
+
+            Console.WriteLine("Analyzing...");
+            ImageAnalysis analysis = await computerVision.AnalyzeImageAsync(url, featureTypes);
+            Console.Clear();
+
+           
+            if (analysis.Objects.Count > 0) //check if any objects is returned
+            {
+                string result = analysis.Objects[0].ObjectProperty; //Result is equal to the top object
+                
+                //Check if the object is a hot dog or not
+                if(result == "Hot dog")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("'||'  '||'           .      '||''|.                   \r\n ||    ||    ...   .||.      ||   ||    ...     ... . \r\n ||''''||  .|  '|.  ||       ||    || .|  '|.  || ||  \r\n ||    ||  ||   ||  ||       ||    || ||   ||   |''   \r\n.||.  .||.  '|..|'  '|.'    .||...|'   '|..|'  '||||. \r\n                                              .|....'");
+                }
+                else {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("'|.   '|'           .      '||'  '||'           .      '||''|.                   \r\n |'|   |    ...   .||.      ||    ||    ...   .||.      ||   ||    ...     ... . \r\n | '|. |  .|  '|.  ||       ||''''||  .|  '|.  ||       ||    || .|  '|.  || ||  \r\n |   |||  ||   ||  ||       ||    ||  ||   ||  ||       ||    || ||   ||   |''   \r\n.|.   '|   '|..|'  '|.'    .||.  .||.  '|..|'  '|.'    .||...|'   '|..|'  '||||. \r\n                                                                         .|....' ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Found object: " + result);
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.WriteLine("Bad image, try again");
+            }
+
+            Console.WriteLine("Press any key to continue");            
+            
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System.Text.RegularExpressions;
+
+namespace SeeFood
+{
+    internal class Program
+    {
+        private static string CognitiveEndpoint = "https://compvisionseefood.cognitiveservices.azure.com/";
+        private static string CognitiveKey = "16fea0ae9134441baf586041c8125192";
+
+        static void Main(string[] args)
+        {
+            ImageAnalyzer imageAnalyzer = new();
+
+            string input = "";
+
+            
+            while (input.ToLower() != "quit")
+            {
+                
+                Console.Write("Insert Image URL: ");
+                input = Console.ReadLine();
+
+                if (input == "quit") return;
+
+                if(IsValidUrl(input))
+                {
+                    imageAnalyzer.DectectItemsAsync(input);
+                    Console.ReadKey();
+                    Console.Clear();
+                }              
+                else
+                {
+                    Console.WriteLine("Invalid URL. Please enter a valid URL.");
+                }                
+            }
+
+            static bool IsValidUrl(string url)
+            {
+                // Define a regular expression pattern for a valid URL
+                string pattern = @"^(https?|ftp)://[^\s/$.?#].[^\s]*$";
+
+                // Use Regex.IsMatch to check if the input matches the pattern
+                return Regex.IsMatch(url, pattern, RegexOptions.IgnoreCase);
+            }
+
+        }
+    }
+}
